53e42bd9625f7212403e1de9bfba8bdd6720f8d5
MohammadMattour <game.mohammad@yahoo.com>
2019-08-08 10:23:43 +0300

    Initial Commit

---
+++ src/main/java/com/vega/springit/repository/LinkRepositoy.java
@@ -0,0 +1,10 @@
+package com.vega.springit.repository;
+
+import com.vega.springit.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepositoy  extends JpaRepository<Link, Long> {
+
+
+
+}
